40db461551a9923e2f54e46b8d42b84725705fe1
Pablo Marchena <pablo.marchena@orbitdevs.com>
2018-08-23 00:09:05 -0600

    Updates

---
+++ precache-manifest.2dcca016fbdef155cdf9eba24c884966.js
@@ -0,0 +1,27 @@
+self.__precacheManifest = [
+  {
+    "url": "vendor.566e083ead6225e53397.js"
+  },
+  {
+    "revision": "0e735631be92bb8b43f7",
+    "url": "runtime~vendor.bundle.js"
+  },
+  {
+    "revision": "494e5cd3e6aec64e2602",
+    "url": "runtime~app.bundle.js"
+  },
+  {
+    "revision": "3784eca1b1f3f845cc987b663b9349e0",
+    "url": "index.html"
+  },
+  {
+    "revision": "349e1f9fc8f47d0dadfdce0f4ed367d6",
+    "url": "favicon.ico"
+  },
+  {
+    "url": "app.bd99c72c251a61f517a2.js"
+  },
+  {
+    "url": "app.bd99c72c251a61f517a2.css"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,3 @@
+importScripts("precache-manifest.2ca677855c8cda6c1e25b16eaa5f63dc.js", "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+
